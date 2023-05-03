2997a4e7395298f2c28bb88eec0e4eb14de7c400
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-05-02 17:49:55 -0700

    Dev update
    Worship live and services pages, splide and motion one dependencies

--- src/utils/format.js
+++ src/utils/format.js
@@ -9,6 +9,24 @@ const formatDate = (dateString) => {
   });
 };
 
+const formatDateShort = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    timeZone: "UTC",
+  });
+};
+
+const getWeekday = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    weekday: "long",
+    timeZone: "UTC",
+  });
+};
+
 const timeFormatter = (timeString) => {
   // Prepend any date, just want timestamp
   const time = new Date("1988-12-23T" + timeString + "Z");
@@ -22,4 +40,22 @@ const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-export { formatDate, timeFormatter, capitalizeFirstLetter };
+const joinNames = (object) => {
+  // mapped to object keys from Sanity content
+  const orderedNames = [
+    object.name.title,
+    object.name.firstName,
+    object.name.lastName,
+  ];
+  const fullName = orderedNames.join(" ");
+  return fullName;
+};
+
+export {
+  formatDate,
+  timeFormatter,
+  capitalizeFirstLetter,
+  joinNames,
+  formatDateShort,
+  getWeekday,
+};
