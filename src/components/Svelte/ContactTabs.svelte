36ad5d3691f4baed4e4c97e7e5e261ce5023955c
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-07-06 17:04:38 -0700

    Dev update
    Contact page + forms, component styles

---
+++ src/components/Svelte/ContactTabs.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from "svelte"
+    import { slide, fade } from 'svelte/transition';
+    import SvelteForm from "./SvelteForm.svelte";
+    import {
+        Tab,
+        TabGroup,
+        TabList,
+        TabPanel,
+        TabPanels,
+    } from "@rgossiaux/svelte-headlessui";
+
+    export let forms;
+    
+</script>
+
+<TabGroup>
+    <TabList class="form-tablist">
+      {#each forms as form (form.id)}
+        <Tab class="form-tab">{form.title}</Tab>
+    {/each}
+  </TabList>
+  <TabPanels>
+    {#each forms as form (form.id)}
+      <TabPanel>
+        <SvelteForm formData={form} />
+      </TabPanel>
+    {/each}
+  </TabPanels>
+</TabGroup>
