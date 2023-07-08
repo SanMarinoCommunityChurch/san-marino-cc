b7c4b2d859ad8d3a1b5f28480e3946aa3764f69a
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-07-08 13:54:01 -0700

    Update scheduled function deploy time

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,7 +4,7 @@ import { schedule } from "@netlify/functions";
 const BUILD_HOOK =
   "https://api.netlify.com/build_hooks/64a9ca02803f813ac1e0bb0a";
 
-const handler = schedule("0 15 * * *", async () => {
+const handler = schedule("0 22 * * *", async () => {
   await fetch(BUILD_HOOK, {
     method: "POST",
   }).then((response) => {
