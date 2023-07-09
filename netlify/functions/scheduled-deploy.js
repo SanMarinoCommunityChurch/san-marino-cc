f9b9843b030fd232470a3921e1cae7106939179f
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-07-08 20:44:47 -0700

    Fix scheduled deploy fetch

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -1,10 +1,10 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 import { schedule } from "@netlify/functions";
 
 const BUILD_HOOK =
   "https://api.netlify.com/build_hooks/64a9ca02803f813ac1e0bb0a";
 
-const handler = schedule("0 22 * * *", async () => {
+const handler = schedule("0 4 * * *", async () => {
   await fetch(BUILD_HOOK, {
     method: "POST",
   }).then((response) => {
