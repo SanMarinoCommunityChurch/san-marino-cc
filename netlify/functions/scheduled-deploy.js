f34d427d265e8923037f23e8ad7e97ca6f983bd9
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2023-11-20 09:10:02 -0800

    Updated scheduled deploy

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -4,8 +4,10 @@ import { schedule } from "@netlify/functions";
 const BUILD_HOOK =
   "https://api.netlify.com/build_hooks/64c2f8eb7b3da1159f3bf745";
 
-// daily at 12:05am UTC-7
-const cronInterval = "5 7 * * *";
+// daily at 12:05am UTC-8
+// chron expression is “At 07:05”
+// for West coast time, -7 in daylight saving time, -8 normally
+const cronInterval = "5 8 * * *";
 
 const handler = schedule(cronInterval, async () => {
   await fetch(BUILD_HOOK, {
