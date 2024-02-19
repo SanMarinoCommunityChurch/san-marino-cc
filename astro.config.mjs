ff60ea88ee5f1acd35cdb9ea27e1af196ba19cfc
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2024-02-19 09:36:04 -0800

    Update Astro v4
    Reinstall svelte integration and remove dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sanmarinocommunitychurch.com",
   scopedStyleStrategy: "class",
   image: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.sanity.io",
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "**.sanity.io"
+    }]
   },
-  integrations: [svelte(), react(), sitemap()],
+  integrations: [react(), sitemap(), svelte()]
 });
